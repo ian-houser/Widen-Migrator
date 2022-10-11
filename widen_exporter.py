24020d8da945d2a445fabc0f5791081f6861f6ce
ian-houser <ian.houser@cbsinteractive.com>
2022-10-11 16:14:56 -0700

    adjusted prints & changed query syntax to be exact

--- widen_exporter.py
+++ widen_exporter.py
@@ -8,8 +8,8 @@ from pathlib import Path
 import sys
 
 ##Widen Parameters (adjustable)
-query = "ag:(vcbs_cm_design_internal)" #Set query from Widen to use to target files- can use advanced searches including AND and OR. More info on advanced search queries: https://community.widen.com/collective/s/article/How-do-I-search-for-assets
-expands = "asset_properties,file_properties,thumbnails,metadata,metadata_info,security,status" #The kinds of extra info to return with results in JSON form. https://widenv2.docs.apiary.io/#reference/expands
+query = "ag:{vcbs_cm_design_internal}" #Set query from Widen to use to target files- can use advanced searches including AND and OR. More info on advanced search queries: https://community.widen.com/collective/s/article/How-do-I-search-for-assets
+expands = "asset_properties,file_properties,thumbnails,metadata,metadata_info,security" #The kinds of extra info to return with results in JSON form. https://widenv2.docs.apiary.io/#reference/expands
 limit = 100 #The number of results to return with each scroll. Accepts numbers 1-100.
 
 #Set working directory as location of the script (run via CMD, not opening script directly)
@@ -37,7 +37,7 @@ scrollApiURL = "https://api.widencollective.com/v2/assets/search/scroll?" + "&ex
 headers = {'Authorization': 'Bearer ' + auth}
 response = requests.get(apiURL, headers=headers).json()
 totalFiles = response['total_count']
-print(str(totalFiles) + " total .json files will be processed.")
+print(str(totalFiles), "total .json files will be processed.")
 scrollID = response['scroll_id']
 
 filesizeCount = 0
@@ -49,7 +49,7 @@ def dumpJSON(items):
         count += 1
         filesizeCount += item['file_properties']['size_in_kbytes']
         filename = item['filename']
-        print("Processing #" + str(count) + "/" + str(totalFiles) + " | " + str(filename))
+        print("Processing #", str(count), "/", str(totalFiles), "|", str(filename))
         jsonFileName = filename + ".json" #extract just the file name from current JSON file for pathlib
         with open(jsonPath / jsonFileName, 'w') as dumpfile:
             json.dump(item, dumpfile, indent = 4 )
@@ -64,4 +64,4 @@ dumpJSON(response['items']) #Dump first batch of .json files before the scroll s
 while count < totalFiles: #Keep calling the API to get more results until we've processed the total number of results
     scrollSearch()
 else:
-    print(str(count) + " .json files have been processed out of " + str(totalFiles) + " | Combined total file size (actual download files, not JSON): " + str(filesizeCount) + " kbytes")
+    print(str(count), ".json files have been processed out of", str(totalFiles), "| Combined total file size (actual download files, not JSON): " + str(filesizeCount) + " kbytes")
