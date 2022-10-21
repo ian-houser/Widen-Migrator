a404ed0f209952ee7b8a947e40fbd095c09171e5
ian-houser <ian.houser@cbsinteractive.com>
2022-10-21 12:58:12 -0700

    Added "update metadata" functionality

--- widen_downloader.py
+++ widen_downloader.py
@@ -1,6 +1,5 @@
 #Looks through a folder of .json files generated from Widen and downloads the original file for each.
-#Version 0.3 2022/10/14 | working | Added functionality to download attachment files
-#Future: update each asset's metadata in Widen to reflect that it's been migrated?
+#Version 0.31 2022/10/21 | working | Added functionality to update Widen metadata for each downloaded asset. Meant to assist in tracking which assets have been downloaded in Widen.
 
 import json
 import requests
@@ -9,19 +8,23 @@ import os
 import glob
 import sys
 from pathlib import Path
+from datetime import date #Only used to update metadata in Widen with 'migrated' date of Today()
+
+##Editable
+#The kinds of extra info to return with results in JSON form. https://widenv2.docs.apiary.io/#reference/expands
+expands = "metadata, asset_properties, file_properties, metadata_info" 
+#The JSON body (as a string) to send back to Widen after the asset has been updated. Initial intent is adding a date to "migrationDate" to indicate when and that it has been downloaded | https://widenv2.docs.apiary.io/#reference/assets/asset-metadata/update-metadata
+update_body = '{"fields": {"migrationDate": [' + str(date.today()) + ']}}'
+update_metadata = True #Set this to False to skip updating metadata of the downloaded asset
 
 #Sets working directory as location of the script
 wd = Path(os.path.realpath(os.path.dirname(__file__)))
 
-##Editable
 #Set auth token. Loads from creds.key file next to script. Contents should be only "xxx/xxxxx..."
 if os.path.exists(wd / "creds.key") == False:
     print("No credential file found! Make a 'creds.key' file next to the script with only your Widen API token inside | 'xxx/xxxxx...'")
     sys.exit()
 auth = open(wd / "creds.key").read().splitlines()[0]
-
-#The kinds of extra info to return with results in JSON form. https://widenv2.docs.apiary.io/#reference/expands
-expands = "metadata, asset_properties, file_properties, metadata_info" 
 
 #sets JSON directory as a "JSON" folder one level deeper than the script file.
 jsonPath = wd / "JSON"
@@ -69,10 +72,24 @@ for file in glob.glob(os.path.join(jsonPath, '*.json')): #only loop through .JSO
             with open(filename, 'wb') as f:
                 fdst = open(destination / filename, 'wb') #wb means "write binary" instead of text
                 shutil.copyfileobj(r.raw, fdst) #streaming download contents to disk at fdst location
-        
+
         #checks for empty downloaded files at root (my IDE left them but terminal didn't)
         if os.path.exists(filename):
             os.remove(filename)#deletes them if they exist. real downloaded files are copied to /downloaded
+        
+        #Update downloaded asset's metadata in Widen
+        if update_metadata == True: #Only fires if user defined variable at top is set to True
+            if update_body != '': #If user defined JSON body is not empty
+                putHeaders = {'Authorization': 'Bearer ' + auth, 'Content-Type': 'application/json'}
+                putURL = 'https://api.widencollective.com/v2/assets/' + fileID + '/metadata'
+                response = requests.put(putURL, data = update_body, headers = putHeaders)
+                if response.status_code == 200:
+                    print('Successfully updated asset metadata in Widen')
+                else: 
+                    print('Error updating metadata:', response.text)
+            else:
+                print('Update_body is empty! Skipping update.')
+
     jsonFileName = Path(str(jsonFile.name)).name #extract just the file name from current JSON file for pathlib
     shutil.move(jsonPath / jsonFileName, jsonPath / "processed" / jsonFileName) # Moving JSON files
 
